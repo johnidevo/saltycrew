7c75667f2509c13ce1b451147cf8e05fd8bfa438
Robert-Dan <danrobert445@gmail.com>
2022-06-24 09:41:16 +0000

    infura

--- test_evm_create.js
+++ test_evm_create.js
@@ -1,6 +1,28 @@
-(async function(){
+(async function(){ //
+const dotenv = require('dotenv').config();
+var request = require('request');
 
-	var ethers = require('ethers');
+var headers = {
+	'Content-Type': 'application/json'
+};
+
+var dataString = '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest",true], "id":1}';
+
+var options = {
+	url: `https://rinkeby.infura.io/v3/${process.env.PROJECT_ID}`,
+	method: 'POST',
+	headers: headers,
+	body: dataString,
+};
+
+function callback(error, response, body) {
+	if (!error && response.statusCode == 200) {
+		json = response.body;
+		var obj = JSON.parse(json);
+		console.log(obj)
+	}
+}
+
+request(options, callback);
 	
-
 })();
