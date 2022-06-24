acad13df6464e1abc60beb543ee3268c26efff67
Robert-Dan <danrobert445@gmail.com>
2022-06-24 12:22:06 +0000

    shell

--- test_evm_create.js
+++ test_evm_create.js
@@ -15,11 +15,11 @@ var headers = {
 //_______________
 	
 	
-var params = {params:[{
+var params = [{
   //"data": "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675",
-}]};
+}];
 
-var dataString = '{"jsonrpc":"2.0","method":"eth_getBlockByNumber",'+ JSON.stringify(params) +',"id":1}';
+var dataString = '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params:"'+ JSON.stringify(params) +',"id":1}';
 
 return console.log(dataString);
 	
