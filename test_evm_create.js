209a037888f4b96d3154cbb735749111499fef4e
Robert-Dan <danrobert445@gmail.com>
2022-06-24 12:58:50 +0000

    shell

--- test_evm_create.js
+++ test_evm_create.js
@@ -16,14 +16,15 @@ var headers = {
 	
 	
 var params = [
+	'0x407d73d8a49eeb85d32cf465507dd71d507100c1',
 	/*{
   	//"data": "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675",
 	},*/
-	//"latest",
+	"latest",
 	//true
 ];
 
-var dataString = '{"jsonrpc":"2.0","method":"eth_accounts","params":'+ JSON.stringify(params) +',"id":1}';
+var dataString = '{"jsonrpc":"2.0","method":"eth_getBalance","params":'+ JSON.stringify(params) +',"id":1}';
 
 console.log(dataString);
 	
