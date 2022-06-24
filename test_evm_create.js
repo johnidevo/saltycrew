20face291969dbf8c81f7797281ead011feb4465
Robert-Dan <danrobert445@gmail.com>
2022-06-24 12:33:09 +0000

    shell

--- test_evm_create.js
+++ test_evm_create.js
@@ -16,14 +16,14 @@ var headers = {
 	
 	
 var params = [
-	{
+	/*{
   	//"data": "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675",
-	},
-	"latest",
-	true
+	},*/
+	//"latest",
+	//true
 ];
 
-var dataString = '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":'+ JSON.stringify(params) +',"id":1}';
+var dataString = '{"jsonrpc":"2.0","method":"eth_coinbase","params":'+ JSON.stringify(params) +',"id":1}';
 
 console.log(dataString);
 	
