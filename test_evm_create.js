5adccad70c92ea7b05381bc561a3135b2f10037f
Robert-Dan <danrobert445@gmail.com>
2022-06-24 12:24:03 +0000

    shell

--- test_evm_create.js
+++ test_evm_create.js
@@ -15,9 +15,13 @@ var headers = {
 //_______________
 	
 	
-var params = [{
-  //"data": "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675",
-}];
+var params = [
+	{
+  	//"data": "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675",
+	},
+	"latest",
+	true
+];
 
 var dataString = '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":'+ JSON.stringify(params) +',"id":1}';
 
